c28604a56d3ea815697325cc0ed95d06b7ab2245
khalkash <khalkash@gmail.com>
2017-10-01 23:56:21 -0400

    got window to popup!

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,56 @@
 package application;
 	
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 
+	@FXML
+	private ListView<songinfo> list;
+	@FXML
+	private AnchorPane root;
+	ArrayList<songinfo> array;
+	
+	
+	
+	
 	
 	
 	@Override
 	public void start(Stage primaryStage) {
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(getClass().getResource("libraryscene.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-
+		SongList songlist = new SongList();
+		array = songlist.getArrayList();
+		songlist.add("Post Malone", "RockStar","", "1963");
+		songlist.add("Hello", "World", "First", "1351");
+		list = new ListView<>();
+		ObservableList<songinfo> ObservArray = FXCollections.observableArrayList(array);
+		list.setItems(ObservArray);
+		
         primaryStage.setTitle("Song Library");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
 			
--- src/application/songinfo.java
+++ src/application/songinfo.java
@@ -1,16 +1,23 @@
 package application;
 
 public class songinfo {
-	String name;
-	String artist;
-	String album;
-	String year;
+	public String name;
+	public String artist;
+	public String album;
+	public String year;
 	songinfo(String name, String artist, String album, String year){
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
 		this.year = year;
 	}
+	
+	@Override
+	public String toString() {
+		return "Song: " + name + " Artist: " + artist;
+	}
+	
+	
 	
 
 }
